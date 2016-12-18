86d8391d06ca4682bbfa96212aa9f8a7cea2adc7
mujaffar <mujaffar.sanadi01@gmail.com>
2016-12-18 23:47:54 +0530

    asdf

--- js/index.js
+++ js/index.js
@@ -1,25 +1,20 @@
 var admobid = {}
 if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
+    alert(7888);
     admobid = {
         banner: 'ca-app-pub-3868593263837372/8649306643',
         interstitial: 'ca-app-pub-3868593263837372/4224486649',
     }
-} else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
-    admobid = {
-        banner: 'ca-app-pub-3940256099942544/2934735716',
-        interstitial: 'ca-app-pub-3940256099942544/4411468910',
-    }
 }
 
 document.addEventListener('deviceready', function () {
-    
-    adbuddiz.setAndroidPublisherKey("TEST_PUBLISHER_KEY_ANDROID");
     
     AdMob.setOptions({
         isTesting: false,
     })
 
     AdMob.createBannerView({
+        isTesting: false,
         autoShow: true,
     });
 
